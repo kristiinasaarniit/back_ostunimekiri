39ee0a9217dde32d7eaf1f7affe907d3dca30c20
Kristiina <kristiina.saarniit@yahoo.com>
2022-11-14 14:18:29 +0200

    esimene kommit

---
+++ src/main/java/ee/valiit/back_ostunimekiri/BackOstunimekiriApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_ostunimekiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOstunimekiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackOstunimekiriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/back_ostunimekiri/BackOstunimekiriApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_ostunimekiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOstunimekiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
